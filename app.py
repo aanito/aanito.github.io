4fe059c487f354b5071c9886bcf785fbf79c656e
aanito <howmarad@gmail.com>
2023-09-07 10:39:31 +0300

    Deployment ports

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def search_hospitals():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
